49dc25aa79371ee5941b363ff6300fb6a675a372
Serhiy Yevtushenko <serhiy.yevtushenko@gmail.com>
2018-07-23 13:56:38 +0300

    Import Catalog page spec

---
+++ spec/features/catalog/catalog_page_spec.rb
@@ -0,0 +1,80 @@
+feature 'Catalog page' do
+  context 'with less than 12 available books' do
+    given(:categories) do
+      ['All', 'Mobile Development', 'Photo',
+       'Web Design', 'Web Development']
+    end
+
+    background do
+      create_list(:book_with_authors_and_materials, 4)
+      visit catalog_index_path
+    end
+
+    scenario 'has catalog filter links' do
+      categories.each do |category|
+        expect(page).to have_css('a.filter-link', text: category)
+      end
+    end
+
+    scenario 'has catalog filter dropdown in xs layout' do
+      categories.each do |category|
+        expect(page).to have_css(
+          '.visible-xs ul.dropdown-menu li a',
+          visible: false, text: category
+        )
+      end
+    end
+
+    scenario 'has 4 book items' do
+      expect(page).to have_css('.general-thumb-wrap', count: 4)
+    end
+
+    context 'click on category link' do
+      scenario 'makes it rendered with bold' do
+        find('a.filter-link', text: 'Mobile Development').click
+        expect(page).to have_css('a.filter-link b', text: 'Mobile Development')
+      end
+
+      scenario 'only leaves books of that category on page' do
+        find('a.filter-link', text: 'Photo').click
+        expect(page).to have_css('.general-thumb-wrap', count: 1)
+      end
+    end
+
+    context 'click on category menu item in xs layout' do
+      scenario 'makes it current menu item' do
+        find('.visible-xs a.dropdown-toggle.dropdown-btn',
+             visible: false, text: 'All').click
+        find('.visible-xs ul.dropdown-menu li a',
+             visible: false, text: 'Web Development').click
+        expect(page).to have_css(
+          '.visible-xs a.dropdown-toggle',
+          visible: false, text: 'Web Development'
+        )
+      end
+
+      scenario 'only leaves books of that category on page' do
+        find('.visible-xs a.dropdown-toggle.dropdown-btn',
+             visible: false, text: 'All').click
+        find('.visible-xs ul.dropdown-menu li a',
+             visible: false, text: 'Web Design').click
+        expect(page).to have_css('.general-thumb-wrap', count: 1)
+      end
+    end
+
+    scenario 'has no view more button' do
+      expect(page).not_to have_link(t('catalog.index.view_more'))
+    end
+  end
+
+  context 'with more than 12 available books' do
+    before do
+      create_list(:book_with_authors_and_materials, 20)
+      visit catalog_index_path
+    end
+
+    scenario 'has view more button' do
+      expect(page).to have_link(t('catalog.index.view_more'))
+    end
+  end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,25 @@
+require 'simplecov'
+SimpleCov.start
+
+require 'rails_helper'
+require 'capybara/rspec'
+require 'capybara/dsl'
+
+%w(support helpers **/shared_examples).each do |folder|
+  # Dir["#{File.dirname(__FILE__)}/../spec/#{folder}/**/*.rb"].each do |file|
+    # require file
+  # end
+  Dir[Rails.root.join("spec/#{folder}/**/*.rb")].each { |file| require file }
+end
+
+RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+  config.shared_context_metadata_behavior = :apply_to_host_groups
+end
