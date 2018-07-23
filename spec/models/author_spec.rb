33150f3585e2eb0812888d801603252bed69b9d5
Serhiy Yevtushenko <serhiy.yevtushenko@gmail.com>
2018-07-23 17:14:33 +0300

    Import Author model spec

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,5 +1,5 @@
-require 'rails_helper'
-
-RSpec.describe Author, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Author do
+  context 'association' do
+    it { is_expected.to have_and_belong_to_many(:books) }
+  end
 end
