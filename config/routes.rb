406d5cecc220323d2d6df049f775a6ebc5b6de67
Serhiy Yevtushenko <serhiy.yevtushenko@gmail.com>
2018-07-27 12:19:19 +0300

    Import layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
-  mount Ecomm::Engine => '/store'
+  root to: 'catalog#index'
+
   devise_for :users
+
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new'
+    get 'logout', to: 'devise/sessions#destroy'
+  end
+
+  mount Ecomm::Engine => '/store'
+
   get 'catalog/index'
+
   resources :books, only: :show
 end
