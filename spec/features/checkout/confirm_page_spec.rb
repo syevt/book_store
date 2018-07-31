6269eb83f7841c77c5767272dd134404bdfe338a
Serhiy Yevtushenko <serhiy.yevtushenko@gmail.com>
2018-07-31 10:03:25 +0300

    Import Checkout Confirm feature spec

--- spec/features/checkout/confirm_page_spec.rb
+++ spec/features/checkout/confirm_page_spec.rb
@@ -1,7 +1,7 @@
 feature 'Checkout confirm page' do
   context 'with guest user' do
     scenario 'redirects to login page' do
-      visit checkout_confirm_path
+      visit ecomm.checkout_confirm_path
       expect(page).to have_content(t('devise.failure.unauthenticated'))
     end
   end
@@ -28,9 +28,9 @@ feature 'Checkout confirm page' do
 
     context 'with no credit card set' do
       scenario 'redirects to checkout payment path' do
-        visit checkout_confirm_path
+        visit ecomm.checkout_confirm_path
         expect(page).to have_css(
-          'h3.general-subtitle', text: t('checkout.payment.credit_card')
+          'h3.general-subtitle', text: t('ecomm.checkout.payment.credit_card')
         )
       end
     end
@@ -49,40 +49,42 @@ feature 'Checkout confirm page' do
           },
           order_subtotal: 5.4
         )
-        visit checkout_confirm_path
+        visit ecomm.checkout_confirm_path
       end
 
       include_examples 'order details'
       include_examples 'extended order details'
 
       scenario 'has addresses edit link' do
-        expect(page).to have_link('edit', href: checkout_address_path)
+        expect(page).to have_link('edit', href: ecomm.checkout_address_path)
       end
 
       scenario 'has shipment edit link' do
-        expect(page).to have_link('edit', href: checkout_delivery_path)
+        expect(page).to have_link('edit', href: ecomm.checkout_delivery_path)
       end
 
       scenario 'has payment edit link' do
-        expect(page).to have_link('edit', href: checkout_payment_path)
+        expect(page).to have_link('edit', href: ecomm.checkout_payment_path)
       end
 
       context 'click on place order' do
         scenario 'with no order errors redirects to order complete page' do
-          click_on(t('checkout.confirm.place_order'))
+          click_on(t('ecomm.checkout.confirm.place_order'))
           expect(page).to have_css(
             'h3.general-subtitle',
-            text: t('checkout.complete.thanks')
+            text: t('ecomm.checkout.complete.thanks')
           )
         end
 
         scenario 'with order errors redirects back to confirm page' do
-          allow_any_instance_of(Order).to receive(:save).and_return(false)
-          click_on(t('checkout.confirm.place_order'))
-          expect(page).to have_content(
-            t('checkout.submit_confirm.order_placement_error')
+          allow_any_instance_of(Ecomm::Order).to(
+            receive(:save).and_return(false)
           )
-          expect(page).to have_button(t('checkout.confirm.place_order'))
+          click_on(t('ecomm.checkout.confirm.place_order'))
+          expect(page).to have_content(
+            t('ecomm.checkout.submit_confirm.order_placement_error')
+          )
+          expect(page).to have_button(t('ecomm.checkout.confirm.place_order'))
         end
       end
     end
