a93daa07cf9d4b24eac55a107db29e5fd9ed6614
Serhiy Yevtushenko <serhiy.yevtushenko@gmail.com>
2018-08-05 15:44:51 +0300

    Fix examples captions for Signup/Login feature specs

--- spec/features/devise/login_page_spec.rb
+++ spec/features/devise/login_page_spec.rb
@@ -5,7 +5,7 @@ feature 'Login page' do
 
   background { create(:user, email: 'valid@example.com') }
 
-  scenario 'with valid data' do
+  scenario 'signs in with valid credentials' do
     create_list(:book_with_authors_and_materials, 4)
     login_form.visit_page.fill_in_with(
       email: 'valid@example.com',
@@ -14,14 +14,14 @@ feature 'Login page' do
     expect(page).to have_content(t('devise.sessions.signed_in'))
   end
 
-  scenario 'with empty email' do
+  scenario 'with empty email shows appropriate error' do
     login_form.visit_page.fill_in_with(password: '11111111').submit
     expect(page).to have_content(
       t('devise.failure.invalid', authentication_keys: 'Email')
     )
   end
 
-  scenario 'with non-existent email' do
+  scenario 'with non-existent email shows invaid email error' do
     login_form.visit_page.fill_in_with(
       email: 'some_email@example.com',
       password: '11111111'
@@ -31,14 +31,14 @@ feature 'Login page' do
     )
   end
 
-  scenario 'with empty password' do
+  scenario 'with empty password shows invalid email error' do
     login_form.visit_page.fill_in_with(email: 'some_email@example.com').submit
     expect(page).to have_content(
       t('devise.failure.invalid', authentication_keys: 'Email')
     )
   end
 
-  scenario 'with invalid password' do
+  scenario 'with invalid password showl invalid email error' do
     login_form.visit_page.fill_in_with(
       email: 'valid@example.com',
       password: '555'
