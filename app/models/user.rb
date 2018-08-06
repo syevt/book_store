8f5f1eb43a0e404f2b6e2fe9f433ffff13555a98
Serhiy Yevtushenko <serhiy.yevtushenko@gmail.com>
2018-08-06 22:32:50 +0300

    Configure FB omniauth with Figaro

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,24 @@ class User < ApplicationRecord
   has_one(:billing_address, -> { billing }, class_name: 'Ecomm::Address',
                                             foreign_key: :customer_id)
   has_many(:orders, class_name: 'Ecomm::Order', foreign_key: :customer_id)
-  has_many(:reviws, dependent: :destroy)
+  has_many(:reviews, dependent: :destroy)
 
   devise(:database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: [:facebook])
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.email = auth.info.email
+      user.addresses << Ecomm::Address.new(
+        first_name: auth.info.first_name,
+        last_name: auth.info.last_name,
+        city: auth.info.hometown,
+        address_type: 'billing'
+      )
+      user.password = Devise.friendly_token[0, 20]
+    end
+  end
 end
