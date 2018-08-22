addf9c2c02b3f46a78e73688ed39e7d8dcdef51d
Serhiy Yevtushenko <serhiy.yevtushenko@gmail.com>
2018-08-23 00:31:47 +0300

    Add Heroku url to production settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,7 +13,7 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
 
   config.action_mailer.default_url_options = {
-    host: 'afternoon-fjord-56962.herokuapp.com' # needs to be changed
+    host: 'polar-crag-11320.herokuapp.com'
   }
 
   # Code is not reloaded between requests.
