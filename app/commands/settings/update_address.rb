38de3deec81aa458fb52f03007daacd30fb7317c
Serhiy Yevtushenko <serhiy.yevtushenko@gmail.com>
2018-11-01 14:43:07 +0200

    Use attr_extras gem to get rid of initializers

--- app/commands/settings/update_address.rb
+++ app/commands/settings/update_address.rb
@@ -1,20 +1,18 @@
 module Settings
   class UpdateAddress < Ecomm::BaseCommand
+    pattr_initialize :find_or_init_address, :get_updated_message
+
     def self.build
       new(Settings::FindOrInitializeAddress.build,
           Settings::GenerateAddressUpdatedMessage.build)
-    end
-
-    def initialize(*args)
-      @find_init_address, @updated_message = args
     end
 
     def call(params, user_id)
       address_type = params.key(t('settings.show.save'))
       address = Ecomm::AddressForm.from_params(params[:address][address_type])
       return publish(:invalid, address) if address.invalid?
-      @find_init_address.call(address.to_h, user_id).save
-      publish(:ok, @updated_message.call(address_type))
+      find_or_init_address.call(address.to_h, user_id).save
+      publish(:ok, get_updated_message.call(address_type))
     end
   end
 end
--- app/services/settings/generate_address_updated_message.rb
+++ app/services/settings/generate_address_updated_message.rb
@@ -1,10 +1,8 @@
 module Settings
   class GenerateAddressUpdatedMessage < Ecomm::BaseService
-    include AbstractController::Translation
-
     def call(type)
       t('settings.show.address_saved',
-        address_type: t("ecomm.checkout.address.#{type}"))
+        address_type: I18n.t("ecomm.checkout.address.#{type}"))
     end
   end
 end
