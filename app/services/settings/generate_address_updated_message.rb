c201b5c8d97c0ae3e0e5c254d43404271dcaa5c4
Serhiy Yevtushenko <serhiy.yevtushenko@gmail.com>
2018-11-06 14:56:33 +0200

    Include lost AbstractController::Translation in GenerateAddressUpdatedMessage

--- app/services/settings/generate_address_updated_message.rb
+++ app/services/settings/generate_address_updated_message.rb
@@ -1,8 +1,10 @@
 module Settings
   class GenerateAddressUpdatedMessage < Ecomm::BaseService
+    include AbstractController::Translation
+
     def call(type)
       t('settings.show.address_saved',
-        address_type: I18n.t("ecomm.checkout.address.#{type}"))
+        address_type: t("ecomm.checkout.address.#{type}"))
     end
   end
 end
